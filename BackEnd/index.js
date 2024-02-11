ee5d7a1f3eb48d76bb8563e86c4e8d22d7291c99
Gouravkarnor <gaurav9karnor@gmail.com>
2024-02-11 14:49:27 +0530

    Backend setup (added dependencies)

---
+++ BackEnd/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello frem server ! ");
+});
+
+app.listen(port, () => {
+  console.log("Connected to ", port);
+});
