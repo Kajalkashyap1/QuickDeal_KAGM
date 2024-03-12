d5ce2deadef4790e8bd3dcd2fa67c68804fdd87d
Gouravkarnor <gaurav9karnor@gmail.com>
2024-03-12 15:51:34 +0530

    login ui addon

--- BackEnd/Controllers/getoneuser.js
+++ BackEnd/Controllers/getoneuser.js
@@ -1,15 +1,18 @@
 const userdata = require("../Models/userdata");
 
 const getuser = async (req, res) => {
-  try {
-    const data = await userdata.find();
-    return res.json({
-      status: "Success",
-      result: data,
-    });
-  } catch (error) {
-    throw error;
-  }
+    try {
+        const data = await userdata.find();
+        res.status(200).json({
+            status: "Success",
+            result: data,
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: "Error",
+        });
+        throw error;
+    }
 };
 
 module.exports = getuser;
--- BackEnd/index.js
+++ BackEnd/index.js
@@ -2,12 +2,16 @@ const express = require("express");
 const app = express();
 const router = require("./Routers/routes");
 const userdata = require("./Models/userdata");
-require("./connections/conn");
+require("./Connections/conn");
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(router);
 
 app.listen(port, () => {
-  console.log(`Connected to ${port}`);
+    console.log(`Connected to ${port}`);
+});
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello !</h1>");
 });
