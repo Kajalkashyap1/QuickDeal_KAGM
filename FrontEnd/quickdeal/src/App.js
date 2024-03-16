73ab2969e8324df500b3821ffbe8ff94340c7f3d
Kajalkashyap1 <kajalkashyap539@gmail.com>
2024-03-16 23:35:51 +0530

    added a form to take the users input

--- FrontEnd/quickdeal/src/App.js
+++ FrontEnd/quickdeal/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./Components/Login/Login";
 import Signin from "./Components/Signup/Signup";
 import Home from "./Components/Dashboard/Home";
+import Products from "./Components/Sell_products/Products";
 function App() {
     return (
         <>
@@ -9,6 +10,7 @@ function App() {
                 <Route exact path="/" Component={Home} />
                 <Route path="/login" Component={Login} />
                 <Route path="/signin" Component={Signin} />
+                <Route path="/sell" Component={Products}/>
             </Routes>
         </>
     );
