0bd95ff7fdd2cb131798f5adad33fcb015d158bf
Kajalkashyap1 <kajalkashyap539@gmail.com>
2024-03-27 18:06:56 +0530

    logo added

--- FrontEnd/quickdeal/src/Components/Header/Header.js
+++ FrontEnd/quickdeal/src/Components/Header/Header.js
@@ -1,9 +1,14 @@
+import { NavLink } from "react-router-dom";
 import style from "./Header.module.css";
+
 
 function Header() {
     return (
-        <div className={style.Header} style={{ padding: "5px" }}>
-            <img src="https://res.cloudinary.com/dsaaqhang/image/upload/ar_16:9,c_fill,e_sharpen,g_auto,h_80,q_auto:best,w_140,z_2/v1711003867/QuickDeal/onlinelogomaker-022024-0033-5725_u3lk5k.png"></img>
+        <div className={style.Header}>
+            <NavLink to="/">
+                <img src="https://res.cloudinary.com/dsaaqhang/image/upload/ar_16:9,c_fill,e_sharpen,g_auto,h_80,q_auto:best,w_140,z_2/v1711003867/QuickDeal/onlinelogomaker-022024-0033-5725_u3lk5k.png"></img>
+            </NavLink>
+
         </div>
     );
 }
--- FrontEnd/quickdeal/src/Components/Navbar/Navbar.js
+++ FrontEnd/quickdeal/src/Components/Navbar/Navbar.js
@@ -2,8 +2,6 @@ import React from "react";
 import "./Navbar.css";
 import { NavLink, useNavigate } from "react-router-dom";
 import axios from "axios";
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import LogoutIcon from '@mui/icons-material/Logout';
 import IconButton from '@material-ui/core/IconButton';
 import { Tooltip } from "@mui/material";
@@ -22,18 +20,19 @@ function Navbar(props) {
             })
             .catch((err) => console.log(err));
     };
+
+    
     return (
         <div className="Header">
-            <h1>QUICK DEAL</h1>
-            {/* <div className="SearchBar">
-                <input type="text" placeholder="Search..." />
-                <button>Search</button>
-            </div> */}
+            <div className="logo">
+                <NavLink to="/">
+                    <img src="https://res.cloudinary.com/dsaaqhang/image/upload/ar_16:9,c_fill,e_sharpen,g_auto,h_80,q_auto:best,w_140,z_2/v1711003867/QuickDeal/onlinelogomaker-022024-0033-5725_u3lk5k.png"></img>
+                </NavLink>
+            </div>
             <div class="InputContainer">
                 <input placeholder="Search.." id="input" class="input" name="text" type="text"/>
 
             </div>
-            {/* <a href="/">Dummy Link</a> */}
             {props.auth.isauth ? (
                 <div >
                     <img
