4548fd33362f707691f01a28f8f676afc99b5f56
Sonalika55 <murmunikita25@gmail.com>
2024-04-25 23:34:07 +0530

    commit

---
+++ FrontEnd/quickdeal/src/Components/Dashboard/Categories.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import CategoryBar from './CategoryBar';
+const Categories = () => {
+  return (
+    <div className='cat-container d-flex justify-content-between'>
+      {/* <div>All Categories</div> 
+      <div>Stationaries Gadgets Fashion Electronics&Appliances</div> */}
+      <CategoryBar/>
+      
+    </div>
+  )
+}
+
+export default Categories;
+
