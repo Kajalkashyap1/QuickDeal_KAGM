82f4e63102566f6e1889f6f409a93bdef12c870a
Gouravkarnor <gaurav9karnor@gmail.com>
2024-04-27 10:37:08 +0530

    added scroll in category

--- FrontEnd/quickdeal/src/Components/Dashboard/CategoryBar.jsx
+++ FrontEnd/quickdeal/src/Components/Dashboard/CategoryBar.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import style from "./Categories.module.css"; // Assuming you've saved the CSS in a file named Categories.css
 import options from "../Assets/categories";
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import Dropdown from 'react-bootstrap/Dropdown';
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import Dropdown from "react-bootstrap/Dropdown";
 import { NavDropdown } from "react-bootstrap";
 import { withWidth } from "@material-ui/core";
 // import { NavDropdown } from "react-bootstrap";
@@ -23,25 +23,43 @@ const CategoryBar = ({ onSelectedCategories2e }) => {
             <div className={style.navbarcontent}>
                 {/* <span>All Category <KeyboardArrowDownIcon/> </span> */}
                 <div>
-                <NavDropdown title="All Categories" className={style.dropDownMenu}>
-                        {options
-                            .sort((a, b) => a.label.localeCompare(b.label)) // Sort options alphabetically by label
-                            .map((item, index) => (
-                                <NavDropdown.Item key={index}>
-                                    <input
-                                        type="checkbox"
-                                        id={item.value}
-                                        onChange={handleCategoryChange}
-                                    />
-                                    <label htmlFor={item.value}>{item.label}</label>
-                                </NavDropdown.Item>
-                            ))}
+                    <NavDropdown
+                        title="All Categories"
+                        show={showDropdown}
+                        onToggle={(isOpen) => setShowDropdown(isOpen)}
+                        className={style.dropDownMenu}>
+                        <div className={style.dropdownScroll}>
+                            {options
+                                .sort((a, b) => a.label.localeCompare(b.label)) // Sort options alphabetically by label
+                                .map((item, index) => (
+                                    <li
+                                        className={style.list}
+                                        key={index}
+                                        onClick={(e) => e.stopPropagation()}
+                                        style={{ cursor: "pointer" }}>
+                                        <input
+                                            type="checkbox"
+                                            id={item.value}
+                                            onChange={handleCategoryChange}
+                                            style={{ cursor: "pointer" }}
+                                        />
+                                        <label
+                                            htmlFor={item.value}
+                                            style={{
+                                                cursor: "pointer",
+                                                marginLeft: "5px",
+                                            }}>
+                                            {item.label}
+                                        </label>
+                                    </li>
+                                ))}
+                        </div>
                     </NavDropdown>
                 </div>
-                
+
                 <div>
                     <ul className={style.category_checkboxes}>
-                        {options.slice(0,6).map((item, index) => {
+                        {options.slice(0, 7).map((item, index) => {
                             return (
                                 <li key={index}>
                                     <input
@@ -49,14 +67,14 @@ const CategoryBar = ({ onSelectedCategories2e }) => {
                                         id={item.value}
                                         onChange={handleCategoryChange}
                                     />
-                                    <label htmlFor={item.value}>{item.label}</label>
+                                    <label htmlFor={item.value}>
+                                        {item.label}
+                                    </label>
                                 </li>
                             );
                         })}
                     </ul>
                 </div>
-               
-                
             </div>
         </nav>
     );
